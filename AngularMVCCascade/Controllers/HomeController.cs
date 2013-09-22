112afc9146956d8f68f02d035f0c0448349860d8
techbrij <techbrij@gmail.com>
2013-09-22 18:13:19 +0530

    Initial Version

---
+++ AngularMVCCascade/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AngularMVCCascade.Models;
+namespace AngularMVCCascade.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+     
+        public JsonResult GetCountries()
+        {
+            
+            using (MyDatabaseEntities context = new MyDatabaseEntities()) {
+                var ret = context.Countries.Select(x=>new { x.CountryId,x.CountryName}).ToList();
+                return Json(ret, JsonRequestBehavior.AllowGet);             
+            }            
+        }
+      
+        [HttpPost]
+        public JsonResult GetStates(int countryId)
+        {
+            using (MyDatabaseEntities context = new MyDatabaseEntities())
+            {
+                var ret = context.States.Where(x => x.CountryId == countryId).Select(x => new { x.StateId, x.StateName }).ToList();
+                return Json(ret);
+            }
+        }
+     
+        public JsonResult GetAll()
+        {
+            using (MyDatabaseEntities context = new MyDatabaseEntities())
+            {
+
+                var ret = context.Countries.Include("State").Select(x => new { x.CountryName, x.CountryId, States = x.States.Select( y=>new { y.StateId,y.StateName }) }).ToList();
+                return Json(ret, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+    }
+}
---
+++ AngularMVCCascade/Scripts/cascade.js
@@ -0,0 +1,46 @@
+﻿var app = angular.module('myModule', []);
+app.controller('myController', function ($scope, $http) {
+
+   GetCountries();  
+    function GetCountries() {
+        $http({
+            method: 'Get',
+            url: '/Home/GetCountries'
+        }).success(function (data, status, headers, config) {
+            $scope.countries = data;
+        }).error(function (data, status, headers, config) {
+            $scope.message = 'Unexpected Error';
+        });
+    }
+
+    $scope.GetStates = function () {
+        var countryId = $scope.country;
+        if (countryId) {
+            $http({
+                method: 'POST',
+                url: '/Home/GetStates/',
+                data: JSON.stringify({ countryId: countryId })
+            }).success(function (data, status, headers, config) {
+                $scope.states = data;
+            }).error(function (data, status, headers, config) {
+                $scope.message = 'Unexpected Error';
+            });
+        }
+        else {
+            $scope.states = null;
+        }
+    }
+    //When you have entire dataset
+    GetAll();
+    function GetAll() {
+        $http({
+            method: 'Get',
+            url: '/Home/GetAll'
+        }).success(function (data, status, headers, config) {
+            $scope.allItems = data;
+        }).error(function (data, status, headers, config) {
+            $scope.message = 'Unexpected Error';
+        });
+    }
+
+});
